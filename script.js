5364e856d39222ed6d5fa02851cfdfe61e5f7de7
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-09 18:33:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+  arr.sort((a,b)=>a-b);
+  let mincost = 0;
+  while(arr.length !== 1){
+	  let sum = (arr[0]+arr[1]);
+	  mincost += sum;
+	  arr.shift();
+	  arr.shift();
+	  arr.push(sum);
+	  arr.sort((a,b) => a-b);
+  }
+
+ return mincost;
   
 }
 
